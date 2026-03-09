ca4c411ee9ce408695c5513abd591e06656b82a4
Chantel Chikanya <chantelchikanya@outlook.com>
2026-03-09 09:35:16 +0200

    build

--- Eyedentity/settings.py
+++ Eyedentity/settings.py
@@ -1,22 +1,19 @@
-import dj_database_url
-import os
 from pathlib import Path
-from django.contrib.messages import constants as messages
+import os
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv(Path(__file__).resolve().parent.parent / '.env')
+except ImportError:
+    pass
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-local-dev-key")
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-this-in-production')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-DEBUG = os.environ.get("DEBUG", "True") == "False"
-
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '.onrender.com',
-    '.fly.dev',
-    'eyedentity.co.zw',
-    'www.eyedentity.co.zw'
-]
+_allowed = os.environ.get('ALLOWED_HOSTS', '')
+ALLOWED_HOSTS = _allowed.split(',') if _allowed else ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -31,7 +28,6 @@ INSTALLED_APPS = [
     'ckeditor_uploader',
     'crispy_forms',
     'crispy_bootstrap5',
-    'storages',
     'apps.main',
     'apps.blog',
 ]
@@ -76,10 +72,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Eyedentity.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eyedentity',
+        'USER': 'chan',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -99,48 +99,13 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
-SUPABASE_PROJECT_ID = 'jmzrevalerojybgvxfas'
-AWS_STORAGE_BUCKET_NAME = 'media'
-AWS_ACCESS_KEY_ID = os.environ.get('SUPABASE_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('SUPABASE_SECRET_ACCESS_KEY')
-AWS_S3_ENDPOINT_URL = f'https://{SUPABASE_PROJECT_ID}.supabase.co/storage/v1/s3'
-AWS_S3_REGION_NAME = 'us-east-1'
-AWS_S3_ADDRESSING_STYLE = "path"
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_FILE_OVERWRITE = False
-AWS_S3_CUSTOM_DOMAIN = f'{SUPABASE_PROJECT_ID}.supabase.co/storage/v1/object/public/{AWS_STORAGE_BUCKET_NAME}'
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-AWS_DEFAULT_ACL = 'public-read'
-AWS_S3_VERIFY = True
-
-if DEBUG:
-    MEDIA_URL = '/media/'
-    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-else:
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SITE_ID = 1
-
-SECURE_SSL_REDIRECT = False
-SECURE_HSTS_SECONDS = 0
-SECURE_HSTS_INCLUDE_SUBDOMAINS = False
-SECURE_HSTS_PRELOAD = False
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
-
-if not DEBUG:
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
 
 CKEDITOR_UPLOAD_PATH = ''
 CKEDITOR_IMAGE_BACKEND = 'pillow'
@@ -223,6 +188,7 @@ SESSION_COOKIE_AGE = 1209600
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 SESSION_SAVE_EVERY_REQUEST = False
 
+from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.DEBUG: 'debug',
     messages.INFO: 'info',
@@ -232,16 +198,14 @@ MESSAGE_TAGS = {
 }
 
 PAGINATE_BY = 12
-
-DEFAULT_META_DESCRIPTION = "Eyedentity Eyewear - Premium eyewear solutions in Harare, Zimbabwe. Stylish, protective, and uniquely you."
-DEFAULT_META_KEYWORDS = "eyewear, glasses, sunglasses, Harare, Zimbabwe, blue light, photochromic, polarized"
-
 FILE_UPLOAD_MAX_MEMORY_SIZE = 5242880
 DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
-
 THUMBNAIL_SIZE = (300, 300)
 PRODUCT_IMAGE_SIZE = (800, 600)
 BLOG_IMAGE_SIZE = (1200, 600)
+
+DEFAULT_META_DESCRIPTION = "Eyedentity Eyewear - Premium eyewear solutions in Harare, Zimbabwe. Stylish, protective, and uniquely you."
+DEFAULT_META_KEYWORDS = "eyewear, glasses, sunglasses, Harare, Zimbabwe, blue light, photochromic, polarized"
 
 if DEBUG:
     INTERNAL_IPS = ['127.0.0.1', 'localhost']
